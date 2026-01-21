3ac4b6ce9cb64b0f8a61ae29dc44ec7a0f3aeacc
Ziyu Li <70241363+ziyuli22@users.noreply.github.com>
2026-01-21 15:59:26 -0700

    HPC material for SHM

---
+++ examples/08-SHM/eval_shm.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from neuralhydrology.evaluation import metrics, get_tester
+import pandas as pd
+from neuralhydrology.utils.config import Config
+
+run_dir = Path("/home/ziyu/neuralhydrology-dcfe/runs/EXP1_SHM_CAMELS_noSnow_HPC_OperationalAvg_2101_143920")
+run_config = Config(run_dir/"config.yml")
+
+# create a tester instance and start evaluation
+tester = get_tester(cfg=Config(run_dir / "config.yml"), run_dir=run_dir, period="test", init_model=True)
+results = tester.evaluate(save_results=True, metrics=run_config.metrics)
