f388f495b515eab94c4a40b484d230d08ab892f3
adam <adammichaelm.p@gmail.com>
2026-03-12 16:24:39 -0600

    Cleaning up branch to merge into master

--- examples/08-SHM/test_saving_conceptual_params.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from neuralhydrology.evaluation.tester import RegressionTester
-from neuralhydrology.utils.config import Config
-
-run_dir = Path("/home/adam/dev/repos/neuralhydrology-dcfe/examples/08-SHM/runs/test_SHM_2402_103807")
-
-tester = RegressionTester(run_dir=run_dir, cfg=Config(run_dir / "config.yml"))
-
-tester.evaluate(save_all_output=True)
