38364c36f963b18ad493226da21a4b3af11f5537
Ziyu Li <70241363+ziyuli22@users.noreply.github.com>
2026-03-12 14:18:41 -0600

    Finished verifying states, params export

--- neuralhydrology/modelzoo/shm.py
+++ neuralhydrology/modelzoo/shm.py
@@ -12,6 +12,16 @@ class SHM(BaseConceptualModel):
     The SHM receives the dynamic parameterization given by a deep learning model. This class has two properties which
     define the initial conditions of the internal states of the model (buckets) and the ranges in which the model
     parameters are allowed to vary during optimization.
+    
+    Conceptual parameters:
+        - dd: snowmelt degree-day factor [mm/°C/day]
+        - f_thr: threshold for fastflow reservoir [mm]
+        - sumax: maximum storage of the unsaturated reservoir [mm]
+        - beta: shape parameter [-]
+        - perc: interflow-baseflow partitioning parameter [%]
+        - kf: fastflow reservoir retention constant [day]
+        - ki: interflow reservoir retention constant [day]
+        - kb: baseflow reservoir retention constant [day]
 
     Parameters
     ----------
@@ -20,10 +30,13 @@ class SHM(BaseConceptualModel):
 
     References
     ----------
-    .. [#] Ehret, U., van Pruijssen, R., Bortoli, M., Loritz, R., Azmi, E., and Zehe, E.: Adaptive clustering: reducing
+        [#] Ehret, U., van Pruijssen, R., Bortoli, M., Loritz, R., Azmi, E., and Zehe, E.: Adaptive clustering: reducing
         the computational costs of distributed (hydrological) modelling by exploiting time-variable similarity among
         model elements, Hydrology and Earth System Sciences, 24, 4389–4411, https://doi.org/10.5194/hess-24-4389-2020,
         2020.
+        [#] Acuña Espinoza, E., Loritz, R., Álvarez Chaves, M., Bäuerle, N., and Ehret, U.: To Bucket or not to Bucket?
+        Analyzing the performance and interpretability of hybrid hydrological models with dynamic parameterization,
+        Hydrology and Earth System Sciences, 28, 2705–2719, https://doi.org/10.5194/hess-28-2705-2024, 2024.
     """
 
     def __init__(self, cfg: Config):
