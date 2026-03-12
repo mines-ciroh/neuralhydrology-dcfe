d5a882b0407118d48f44bc6049a65501b3ed4655
adam <adammichaelm.p@gmail.com>
2026-03-12 17:03:02 -0600

    configs, evaluation, training scripts (if any) should be stored in manuscript folder

--- examples/08-SHM/run_shm.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-import torch
-from neuralhydrology.nh_run import start_run
-
-# by default we assume that you have at least one CUDA-capable NVIDIA GPU
-if torch.cuda.is_available():
-    print("GPU")
-    start_run(config_file=Path("/home/adam/dev/repos/neuralhydrology-dcfe/examples/08-SHM/Exp1_SHM_CAMELS_HPC.yml"))
