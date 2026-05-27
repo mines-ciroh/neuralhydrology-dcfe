dd363db77b35fd05c4510ed9907f5090ae0f4e5a
Ziyu Li <70241363+ziyuli22@users.noreply.github.com>
2026-05-27 11:27:04 -0600

    Getting it ready for devcon26

--- manuscript/code/train_model.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-import torch
-from neuralhydrology.nh_run import start_run
-
-# by default we assume that you have at least one CUDA-capable NVIDIA GPU
-if torch.cuda.is_available():
-    print("GPU")
-    start_run(config_file=Path("/home/ziyu/neuralhydrology-dcfe/manuscript/config/531basin_snow_dcfe_hpc.yml"))
